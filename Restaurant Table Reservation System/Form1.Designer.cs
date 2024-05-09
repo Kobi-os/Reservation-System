2c481d61a259f017073dcf41b6de95ce6cf280f3
Wojtek <wojtek.kobier@wp.pl>
2024-05-09 21:04:46 +0200

    slight changes

--- Restaurant Table Reservation System/Form1.Designer.cs
+++ Restaurant Table Reservation System/Form1.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             titleBox = new TextBox();
             panel1 = new Panel();
-            pictureBox1 = new PictureBox();
             label1 = new Label();
+            pictureBox1 = new PictureBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            panel2 = new Panel();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            menuTransition = new System.Windows.Forms.Timer(components);
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // titleBox
@@ -63,6 +73,17 @@
             panel1.Size = new Size(1047, 38);
             panel1.TabIndex = 2;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(41, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 21);
+            label1.TabIndex = 4;
+            label1.Text = "Pizzeria Parmegianna";
+            label1.Click += label1_Click;
+            // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
@@ -73,15 +94,79 @@
             pictureBox1.TabIndex = 3;
             pictureBox1.TabStop = false;
             // 
-            // label1
+            // flowLayoutPanel1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(48, 11);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 4;
-            label1.Text = "label1";
-            label1.Click += label1_Click;
+            flowLayoutPanel1.BackColor = Color.Black;
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(button4);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 38);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Padding = new Padding(0, 30, 0, 0);
+            flowLayoutPanel1.Size = new Size(217, 501);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(-15, -10);
+            button1.Name = "button1";
+            button1.Size = new Size(156, 62);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.LightCoral;
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(128, 38);
+            panel2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Black;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(29, 28);
+            button2.Name = "button2";
+            button2.Size = new Size(162, 40);
+            button2.TabIndex = 4;
+            button2.Text = "Menu";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Black;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(29, 120);
+            button3.Name = "button3";
+            button3.Size = new Size(162, 40);
+            button3.TabIndex = 5;
+            button3.Text = "O nas";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Black;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(29, 74);
+            button4.Name = "button4";
+            button4.Size = new Size(162, 40);
+            button4.TabIndex = 6;
+            button4.Text = "Zarezerwuj Stolik";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // menuTransition
+            // 
+            menuTransition.Tick += menuTransition_Tick;
             // 
             // Form1
             // 
@@ -91,6 +176,7 @@
             BackColor = Color.Coral;
             BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(1047, 539);
+            Controls.Add(flowLayoutPanel1);
             Controls.Add(panel1);
             Controls.Add(titleBox);
             DoubleBuffered = true;
@@ -100,6 +186,8 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -110,5 +198,12 @@
         private Panel panel1;
         private Label label1;
         private PictureBox pictureBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Panel panel2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private System.Windows.Forms.Timer menuTransition;
     }
 }
--- Restaurant Table Reservation System/Form1.cs
+++ Restaurant Table Reservation System/Form1.cs
@@ -11,5 +11,33 @@ namespace Restaurant_Table_Reservation_System
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool menuExpand = false;
+
+        private void menuTransition_Tick(object sender, EventArgs e)
+        {
+            if (menuExpand == false) {
+                menuContainer.Height += 18;
+                if(menuContainer.Height >= 165)
+                {
+                    menuTransition.Stop ();
+                    menuExpand = false;
+                }
+            }
+            else
+            {
+                menuContainer.Height -= 18;
+                if(menuContainer.Height <= 53)
+                {
+                    menuTransition.Stop ();
+                    menuExpand = true;
+                }
+            }
+        }
     }
 }
